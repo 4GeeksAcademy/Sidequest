62c049f1545feb56276e7568b65de3301799667a
Alejandro Vielma Rondon <alejandrovir22@gmail.com>
2026-05-20 00:13:21 +0000

    Login, Recover, register

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -1,5 +1,4 @@
 // Import necessary components and functions from react-router-dom.
-
 import {
     createBrowserRouter,
     createRoutesFromElements,
@@ -9,6 +8,11 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+
+// Importamos las nuevas vistas de autenticación
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { RecoverPassword } from "./pages/RecoverPassword";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -22,9 +26,14 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-        <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
+        <Route path="/" element={<Home />} />
+        <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+        
+        {/* Nuevas rutas de Autenticación agregadas */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recover-password" element={<RecoverPassword />} />
       </Route>
     )
 );
